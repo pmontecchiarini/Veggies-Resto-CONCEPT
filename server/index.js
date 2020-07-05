331e26fa605d863c5a874e8cfc2f0732a1128d90
Pato <pmontecchiarini@gmail.com>
2020-07-05 00:25:09 -0300

    Security reasons.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const routes = require("./routes");
 const path = require("path");
 const bodyParser = require("body-parser");
-// const nodemailer = require("nodemailer");
+
 
 require('dotenv').config({ path: 'variables.env'});
 
@@ -39,48 +39,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Load routes
 app.use("/", routes());
 
-//Contact form
-// app.post("/send", (req, res) => {
-// const output = `<p>You have a new contact request</p>
-// <h3>Contact detail</h3>
-// <ol>
-//   <li>Name: ${req.body.name} </li>
-//   <li>Email: ${req.body.email}</li>
-// </ol>
-// <h3>Message</h3>
-// <p>Message: ${req.body.message}</p>`
-//     ;
-//   async function main() {
-//     // create reusable transporter object using the default SMTP transport
-//     let transporter = nodemailer.createTransport({
-//       host: "smtp.gmail.com",
-//       port: 465,
-//       secure: true, // true for 465, false for other ports
-//       auth: {
-//         user: "pmontecchiarini.dev@gmail.com", // generated ethereal user
-//         pass: "Mulder$$0128", // generated ethereal password
-//       },
-//     });
-
-//     // send mail with defined transport object
-//     let info = await transporter.sendMail({
-//       from: '"Veggies Takeout Contact Form" <pmontecchiarini.dev@gmail.com>', // sender address
-//       to: "pato1025@gmail.com", // list of receivers
-//       subject: "You have a message from Veggies Resto", // Subject line
-//       text: "Testing", // plain text body
-//       html: output, // html body
-//     },
-//     { tls: {rejectUnauthorized:false}
-//     }
-//     );
-
-//     console.log("Message sent: %s", info.messageId);
-//     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
-//     res.render("/contactus", { msg: "Your message has been sent" });
-//   }
-//   main().catch(console.error);
-// });
 
 /** PORT & HOST */
 const host = process.env.HOST || '0.0.0.0';
